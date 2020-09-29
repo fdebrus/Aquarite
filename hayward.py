ced48b2d551683884fa891ba945cd692bc686250
fdebrus <33791533+fdebrus@users.noreply.github.com>
2020-09-29 14:05:20 +0200

    Update hayward.py

--- hayward.py
+++ hayward.py
@@ -8,7 +8,7 @@ from requests import Session
 import argparse
 
 # Hayward PoolWatch Credentials
-username='<email>'
+username='<username>'
 password='<password>'
 
 # Pool Param
@@ -17,7 +17,7 @@ hasPH='true' # PH Probe
 hasRX='true' # RX Probe
 hasCL='false' # ??
 hasCD='false' # ??
-hasHidro='true'
+hasHidro='true' 
 hasLight='true' # Has relay for light
 hasRelays='true' # Has more relays
 numRelays='1,2,3' # How many extra relay are present
@@ -29,7 +29,7 @@ needsTimeBesgoRemaining='false' # ??
 
 # PoolWatch URL's
 authURL = 'http://poolwatch.hayward.fr/fr/login/login'
-queryURL = 'http://poolwatch.hayward.fr/fr/pool/getmainvalues?id='+poolId+'&hasPH='+hasPH+'&hasRX='+hasRX+'&hasCL='+                                                                                                                         hasCL+'&hasCD='+hasCD+'&hasHidro='+hasHidro+'&hasLight='+hasLight+'&hasRelays='+hasRelays+'&numRelays='+numRelays+'&                                                                                                                         hasFiltration='+hasFiltration+'&hasBackwash='+hasBackwash+'&hasIO='+hasIO+'&hasUV='+hasUV+'&needsTimeBesgoRemaining=                                                                                                                         '+needsTimeBesgoRemaining
+queryURL = 'http://poolwatch.hayward.fr/fr/pool/getmainvalues?id='+poolId+'&hasPH='+hasPH+'&hasRX='+hasRX+'&hasCL='+hasCL+'&hasCD='+hasCD+'&hasHidro='+hasHidro+'&hasLight='+hasLight+'&hasRelays='+hasRelays+'&numRelays='+numRelays+'&hasFiltration='+hasFiltration+'&hasBackwash='+hasBackwash+'&hasIO='+hasIO+'&hasUV='+hasUV+'&needsTimeBesgoRemaining='+needsTimeBesgoRemaining 
 lightURL = 'http://poolwatch.hayward.fr/fr/pool/setlightvalues?id='+poolId+'&type=0&status='
 relayURL = 'http://poolwatch.hayward.fr/fr/pool/setauxvalues?id='+poolId
 pumpURL = 'http://poolwatch.hayward.fr/fr/pool/setfiltrationvalues?id='+poolId
@@ -44,13 +44,13 @@ class HaywardConnectPool(object):
             try:
                 print('Connecting to Hayward PoolWatch - ', end = '')
                 login_data = {'user':username,'pass':password,'entrar':'Entrer'}
-                response = s.post(authURL,login_data) # need to manage in case of error (code to be written) success                                                                                                                          login or not give 200
+                response = s.post(authURL,login_data) # need to manage in case of error (code to be written) success login or not give 200
                 print('Connected')
             except Exception as e:
                 print('Error: %s' % e)
                 exit(1)
         return s
-
+    
     def ohPutValue(self, item, value):
         try:
             rc =requests.put('http://'+OpenHABIP+'/rest/items/'+item+'/state', str(value))
@@ -68,7 +68,7 @@ class HaywardConnectPool(object):
         except Exception as e:
             print('Error: %s' % e)
             exit(1)
-
+    
     def setPump(self, session, params):
         try:
             print('Pump mode' + params)
@@ -77,7 +77,7 @@ class HaywardConnectPool(object):
         except Exception as e:
             print('Error: %s' % e)
             exit(1)
-
+    
     def setAux(self, session, aux, status):
         try:
             print('Setting AUX ' + aux + ' to ' + status)
@@ -87,7 +87,7 @@ class HaywardConnectPool(object):
             exit(1)
 
     def queryData(self, session, arg):
-        if (arg == 'dashboard'):
+        if (arg == 'dashboard'): 
            try:
               print('Dashboard for PoolID ' + poolId)
               json_string = session.get(queryURL+'&config=0')
@@ -101,8 +101,8 @@ class HaywardConnectPool(object):
            except Exception as e:
               print('Error: %s' % e)
               exit(1)
-
-        print(json_string.content)
+            
+        print(json_string.content) 
 
         data = json.loads(json_string.content)
         if (data != 'not connected'): # Pool is not connected to PoolWatch
@@ -138,16 +138,16 @@ def main():
 
     parser = argparse.ArgumentParser(usage)
 
-    parser.add_argument('-r', action='store', dest='refresh', choices=['dashboard', 'config'], help='request refresh                                                                                                                          of dashboard or config data')
-    parser.add_argument('-l', action='store', dest='light', choices=['0','1'], help='set light status to 0:OFF or 1:                                                                                                                         ON')
-    parser.add_argument('-a', action='append', nargs=2, dest='aux', choices=['0','1','2','3'], help='set aux relay s                                                                                                                         tatus to 0:OFF or 1:ON')
-    parser.add_argument('-p', action='store', dest='pumpMode', choices=['MANU','AUTO','HEAT','SMART','INTEL'], help=                                                                                                                         'set filtration mode')
+    parser.add_argument('-r', action='store', dest='refresh', choices=['dashboard', 'config'], help='request refresh of dashboard or config data')
+    parser.add_argument('-l', action='store', dest='light', choices=['0','1'], help='set light status to 0:OFF or 1:ON')
+    parser.add_argument('-a', action='append', nargs=2, dest='aux', choices=['0','1','2','3'], help='set aux relay status to 0:OFF or 1:ON')
+    parser.add_argument('-p', action='store', dest='pumpMode', choices=['MANU','AUTO','HEAT','SMART','INTEL'], help='set filtration mode')
     parser.add_argument('-t', action='store', dest='pumpTemp', default='28', help='set target temperature')
     parser.add_argument('-c', action='store', dest='pumpClim', default='1', help='set climatisation 0:OFF or 1:ON')
-    parser.add_argument('-f', action='store', dest='pumpFreeze', default='0', help='set freeze prevention 0:OFF or 1                                                                                                                         :ON')
-    parser.add_argument('-d', action='store', dest='pumpDuration', default='1380', help='set intelligent mode minimu                                                                                                                         m filtration time') # 1380 = 24HRS, how ?
-    parser.add_argument('-i', action='store', dest='pumpInterval', default='"inter1":"07:00-22:00","inter2":"00:00-0                                                                                                                         0:00","inter3":"00:00-00:00"', help='set interval for filtration')
-
+    parser.add_argument('-f', action='store', dest='pumpFreeze', default='0', help='set freeze prevention 0:OFF or 1:ON')
+    parser.add_argument('-d', action='store', dest='pumpDuration', default='1380', help='set intelligent mode minimum filtration time') # 1380 = 24HRS, how ?
+    parser.add_argument('-i', action='store', dest='pumpInterval', default='"inter1":"07:00-22:00","inter2":"00:00-00:00","inter3":"00:00-00:00"', help='set interval for filtration')
+    
     args = parser.parse_args()
 
     c = HaywardConnectPool()
@@ -158,10 +158,10 @@ def main():
         returnVal = c.queryData(s,args.refresh)
     elif args.light:
         print('Executing: LIGHT')
-        returnVal = c.setLight(s,args.light)
+        returnVal = c.setLight(s,args.light) 
     elif args.aux:
         print('Executing: AUX')
-        returnVal = c.setAux(s,args.aux[0][0],args.aux[0][1])
+        returnVal = c.setAux(s,args.aux[0][0],args.aux[0][1]) 
     elif args.pumpMode:
         print('Executing: PUMP')
         pumpType = str(c.pumpDecode(args.pumpMode))
@@ -172,17 +172,17 @@ def main():
             params = '&data={"type":'+pumpType+'}&int={'+options.pumpInterval+'}&vel='
             print(params)
         if (pumpType == '2'):
-            params = '&data={"type":'+pumpType+',"temp":"'+options.pumpTemp+'","clima":'+options.pumpClim+'}&int={'+                                                                                                                         options.pumpInterval+'}&vel='
+            params = '&data={"type":'+pumpType+',"temp":"'+options.pumpTemp+'","clima":'+options.pumpClim+'}&int={'+options.pumpInterval+'}&vel='
             print(params)
         if (pumpType == '3'):
-            params = '&data={"type":'+pumpType+',"tempMin":20,"tempMax":'+options.pumpTemp+',"freeze":'+options.pump                                                                                                                         Freeze+'}&int={'+options.pumpInterval+'}&vel='
+            params = '&data={"type":'+pumpType+',"tempMin":20,"tempMax":'+options.pumpTemp+',"freeze":'+options.pumpFreeze+'}&int={'+options.pumpInterval+'}&vel='
             print(params)
         if (pumpType == '4'):
-            params = '&data={"type":'+pumpType+',"temp":"'+options.pumpTemp+'","time":'+options.pumpDuration+'}&int=                                                                                                                         &vel='
+            params = '&data={"type":'+pumpType+',"temp":"'+options.pumpTemp+'","time":'+options.pumpDuration+'}&int=&vel='
             print(params)
-        returnVal = c.setPump(s,params)
+        returnVal = c.setPump(s,params) 
     else :
-        returnVal=2
+        returnVal=2 
 
     exit(returnVal)
 
